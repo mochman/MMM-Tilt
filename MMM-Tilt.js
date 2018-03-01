12e28b03fc8e0971807d94d9a9e2b0462933ac4c
mochman <lukemoch@gmail.com>
2018-02-28 21:27:21 -0500

    Initial Upload

--- MMM-Tilt.js
+++ MMM-Tilt.js
@@ -1,33 +1,107 @@
 Module.register("MMM-Tilt", {
-	defaults: {
-		URL: "",
-		login: "",
-		token: "",
-		updateInterval: 1000 * 3600, //Update every hour
-		animationSpeed: 1000 * 2,
-		initialLoadDelay: 1000 * 5
-	},
-	//Define required scripts
+    defaults: {
+        URL: "",
+        updateInterval: 1000 * 3600, //Update every hour
+        animationSpeed: 1000 * 2,
+        initialLoadDelay: 1000 * 5
+    },
+
+    //Define required scripts
 	getStyles: function () {
-		return ["MMM-Tilt.css"];
-	},
+        return ["MMM-Tilt.css"];
+    },
 
-	start: function () {
-	},
+    start: function () {
+		console.log("Starting Module: " + this.name);
+		this.loaded = null;		
 
-	//Override dom generator
-	getDom: function() {
-	},
+		this.gravity = null;
+		this.temperature = null;
+		this.timepoint = null;
+		this.color = null;
+		this.beerName = null;
 
-	scheduleUpdate: function(delay) {
-		var nextLoad = this.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
+		this.scheduleUpdate(this.config.initialLoadDelay);
+    },
+
+    //Override dom generator
+    getDom: function() {
+		var wrapper = document.createElement("div");
+		if (!this.loaded) {
+			wrapper.innerHTML = "Loading...";
+			wrapper.className = "dimmed light small";
+			return wrapper;
 		}
+
+		var table = document.createElement("table");
+		table.classList.add("xsmall", "table");
+		var headerRow = document.createElement("tr");
+		var nameLabel = document.createElement("th");
+		nameLabel.innerHTML = "Beer Name";
+		headerRow.appendChild(nameLabel);
+		var tempLabel = document.createElement("th");
+        tempLabel.innerHTML = "Temp";
+		tempLabel.className = "center";
+        headerRow.appendChild(tempLabel);
+		var gravLabel = document.createElement("th");
+        gravLabel.innerHTML = "Gravity";
+		gravLabel.className = "center";
+        headerRow.appendChild(gravLabel);
+		table.appendChild(headerRow);
+
+		var row = document.createElement("tr");
+		var nameCell = document.createElement("td");
+		nameCell.innerHTML = this.beerName;
+		row.appendChild(nameCell);
+		var tempCell = document.createElement("td");
+		tempCell.innerHTML = this.temperature + "&deg;";
+		tempCell.className = "center";
+        row.appendChild(tempCell);
+		var gravCell = document.createElement("td");
+        gravCell.innerHTML = this.gravity;
+        gravCell.className = "center";
+        row.appendChild(gravCell);
+		table.appendChild(row);
+
+		wrapper.appendChild(table);
+		return wrapper;
+    },
+
+    getData: function () {
 		var self = this;
-		setTimeout(function() {
-			self.SOMEFUNCTION(); //function to repeat
-		}, nextLoad);
+		var requestURL = self.config.URL + "?getData=true";
+		var newRequest = new XMLHttpRequest();
+		newRequest.open("GET", requestURL, true);
+		newRequest.onreadystatechange = function () {
+			if (newRequest.readyState === 4) {
+				if (newRequest.status === 200) {
+					self.processData(JSON.parse(newRequest.response));
+				}
+			}
+		};
+		newRequest.send();        
+    },
+
+	processData: function(data) {
+		console.log(data);
+		this.timepoint = data.Timepoint;
+		this.beerName = data.Beer;
+		this.gravity = data.SG;
+		this.temperature = data.Temp;
+		this.color = data.Color;
+		this.loaded = true;
+		this.updateDom(this.config.animationSpeed);
 	},
+
+    scheduleUpdate: function(delay) {
+        var nextLoad = this.config.updateInterval;
+        if (typeof delay !== "undefined" && delay >= 0) {
+            nextLoad = delay;
+        }
+        var self = this;
+        setTimeout(function() {
+            self.getData(); //function to repeat
+        }, nextLoad);
+    },
 
 });
